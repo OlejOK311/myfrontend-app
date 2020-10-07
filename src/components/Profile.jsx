8145d668c5d3da0c127083088781a8878ee3a756
oplak <oplak@yandex.ru>
2020-10-07 17:08:36 +0300

    init

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='http://rzncit.ru/media/k2/items/cache/8b072d4f47fa16702f8c7356d2c88ee8_M.jpg' />
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
