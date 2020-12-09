040485f7f7cb20fcbcc258f54e3eb81687780e8b
oplak <oplak@yandex.ru>
2020-12-09 16:30:45 +0300

    35

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import {rerenderEntireTree} from '../render';
+let rerenderEntireTree = () => {
+    
+}
 
 let state = {
     
@@ -6,7 +8,8 @@ let state = {
       posts: [
           {id:1,message:'Hi, how are you?',likesCount:17},
           {id:2,message:'It\'s my first post.',likesCount:3}
-      ]
+      ],
+      newPostText: ''
     },
 
     dialogsPage: {
@@ -29,14 +32,24 @@ let state = {
 
 }
 
-export let addPost = (postMessage) => {
+export const addPost = () => {
     let newPost = {
         id: 5,
-        message: postMessage,
+        message: state.profilePage.newPostText,
         likesCount: 0
     };
     state.profilePage.posts.push(newPost);
+    state.profilePage.newPostText = '';
     rerenderEntireTree(state);
+}
+
+export const updateNewPostText = (newText) => {
+    state.profilePage.newPostText = newText;
+    rerenderEntireTree(state);
+}
+
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer;
 }
 
 export default state;
